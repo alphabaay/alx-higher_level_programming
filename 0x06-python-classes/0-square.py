6a14609e54a0aeab212f95aa3cea263f61697efb
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-05 12:47:55 +0300

    python exceptions and data structures

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This code creates an empty Square class, which does nothing
+pass argument ensures the class does nothing"""
+
+
+class Square:
+    """The class does nothing"""
+
+    pass
